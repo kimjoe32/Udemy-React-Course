05701582e0576290801c043721bcbd851b16cf5c
Joe Kim <kimjoe32@gmail.com>
2018-03-15 12:40:00 -0400

    starting project 4

--- ReduxSimpleStarter-2/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-	return [
-		{title: 'JS: The Good Parts', pages: 13},
-		{title: 'HP', pages: 123},
-		{title: 'Dark Tower', pages: 2123},
-		{title: 'Eloquent Ruby', pages: 9}
-	];
-}
